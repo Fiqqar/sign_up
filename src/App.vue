fbbebfa742de01e47a659b49a2f542de8c37ea05
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-05-03 20:29:41 +0700

    3-5-24 (updating App.vue)
    debugging

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
-<template>
+<script setup>
+    import {Nav, Alert} from '@/components/'
+    import { useAuthStore } from './stores/auth.store';
+    const authStore = useAuthStore();
+</script>
 
+<template>
+    <div class="app-container" :class="authStore.user && 'bg-light'">
+        <Nav></Nav>
+        <Alert/>
+        <div class="container pt-4 pb-4">
+            <router-view></router-view>
+        </div>
+    </div>
 </template>
 
-<script>
 
-</script>
 
 <style>
 @import '@/assets/main.css';
